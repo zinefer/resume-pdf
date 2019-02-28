10644fe6941c4999303c5bd70550806098b06f2f
zinefer <zinefer@gmail.com>
2019-02-28 14:45:10 -0700

    Add sass support and normalize.css

--- serve.js
+++ serve.js
@@ -1,25 +1,36 @@
-var path = require('path'),
-    express = require('express'),
+var path     = require('path'),
+    express  = require('express'),
     chokidar = require('chokidar'),
-    reload = require('reload');
+    reload   = require('reload'),
+    sass     = require('node-sass-middleware');
 
 var watcher = chokidar.watch('src');
 
 var app = express()
             .set('view engine', 'pug')
-            .set('views', path.join(__dirname, '/src'));
+            .set('views', 'src');
 
 var reloadServer = reload(app);
 
 watcher.on('ready', () => {
   watcher.on('all', (id, path) => {
-    console.log(`Reload for ${path}`);
+    console.log(`Change detected in ${path}`);
     reloadServer.reload();
   });
 });
 
+app.use(
+  sass({
+    src: path.join(__dirname, 'src/sass'),
+    dest: path.join(__dirname, 'dist'),
+    debug: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use((req, res, next) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+   res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
 
 app.listen(8080);
