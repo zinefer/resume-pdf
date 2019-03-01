073e18596fb2f27fffb4478a1f299fb03b0e6310
zinefer <zinefer@gmail.com>
2019-03-01 11:11:43 -0700

    Calculate years of experience

--- data.js
+++ data.js
@@ -3,10 +3,16 @@ const fs   = require('fs'),
 
 var data = yaml.safeLoad(fs.readFileSync('src/credentials.yml', 'utf8'));
 
+// Calculate the longest list to assist the layout
 var professionalLen = data.Skills.Professional.length,
     proficientLen   = data.Skills.Proficient.length;
     familiarLen     = data.Skills.Familiar.length;
-
 data.SkillsMaxLength = Math.max(professionalLen, proficientLen, familiarLen);
+
+// Calculate years of experience
+var firstJob  = data.Experience.length-1,
+    workingMS = Date.now() - new Date(data.Experience[firstJob].HireDate).getTime(),
+    years     = Math.abs(new Date(workingMS).getUTCFullYear() - 1970);
+data.YearsExperience = years
 
 module.exports = data;
